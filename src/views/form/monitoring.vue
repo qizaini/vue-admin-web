0f7711a067c6b86e36285d61089d827781676e9a
qizaini <18374069947@163.com>
2019-10-29 15:06:35 +0800

    slider over commit

--- src/views/form/monitoring.vue
+++ src/views/form/monitoring.vue
@@ -10,7 +10,20 @@
       </div>
     </div>
 
-    <div class="div-monitoring">
+    <div class="div-monitoring-top">
+      <div class="monitoring-left">
+        <h2>方案介绍</h2>
+        <span>
+          而库区内部的所有要素，包括人员、叉车、货物、托盘等都需要佩戴UWB定位标签，
+          所有的定位数据都回被输送到定位引擎通过外部接口提供给WMS仓储管理系统进行进一步整合。
+        </span>
+      </div>
+      <div class="monitoring-right">
+        <img src="../../img/carouselMap3.png" height="150"/>
+      </div>
+    </div>
+
+    <div class="div-monitoring-center">
       <div class="monitoring-left">
         <h2>方案介绍</h2>
         <span>
@@ -20,6 +33,19 @@
       </div>
       <div class="monitoring-right">
         <img src="../../img/carouselMap2.png" height="150"/>
+      </div>
+    </div>
+
+    <div class="div-monitoring-bottom">
+      <div class="monitoring-left">
+        <h2>方案介绍</h2>
+        <span>
+          而库区内部的所有要素，包括人员、叉车、货物、托盘等都需要佩戴UWB定位标签，
+          所有的定位数据都回被输送到定位引擎通过外部接口提供给WMS仓储管理系统进行进一步整合。
+        </span>
+      </div>
+      <div class="monitoring-right">
+        <img src="../../img/carouselMap5.png" height="150"/>
       </div>
     </div>
 
